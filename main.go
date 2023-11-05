cd7ffb0f2e54a60d553debd4d287c7fd93531129
Chris Grayson <chris@activeprospect.com>
2023-11-05 11:55:18 -0600

    basic, working micro.blog posts

--- main.go
+++ main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"log"
 )
 
+type Message struct {
+	From            string
+	Text            string
+	NumImages       int
+	TwilioImageURLs []string
+	ImageFilenames  []string
+	MBImageURLs     []string
+	MBPostURL       string
+}
+
+var config Config
+var TestTwilioMsg string
+
 func main() {
-	fmt.Println("hi")
+	// load this at the start of each request
+	config = LoadConfig()
+
+	/*
+		- listen for webhook post from Twilio
+		- parse webhook post from Twilio (-> create Message, with From, Text, & Twilio Image URLs)
+		- download any images to local temp files (-> add Image Filenames to Message)
+		- upload any images to Micro.blog (-> add MB Image URLs to Message)
+		- post actual message to Micro.blog (-> add MB Post URL to Message)
+		- upload images & post message to Twitter
+		- respond to Twilio
+	*/
+	message := ParseTwilioWebhook(TestTwilioMsg)
+
+	if message.NumImages > 0 {
+		err := DownloadTwilioImages(&message)
+		if err != nil {
+			log.Fatal("Error downloading from Twilio; exiting")
+		}
+
+		err = UploadImagesToMicroBlog(&message)
+		if err != nil {
+			log.Fatal("Error uploading images to Micro.blog; exiting")
+		}
+	}
+
+	err := UploadMessageToMicroBlog(&message)
+	if err != nil {
+		log.Fatal("Error posting message to Micro.blog; exiting")
+	}
 }
--- microblog.go
+++ microblog.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -13,11 +15,32 @@ import (
 	"strings"
 )
 
+func destinationBlog(message *Message) (destination string) {
+	destination = config.MicroBlog.Destination
+	if strings.HasPrefix(message.Text, "TEST: ") {
+		destination = config.MicroBlog.TestDestination
+	}
+	return
+}
+
+func newMbRequest(mpDestination string, media bool, body io.Reader) (*http.Request, error) {
+	mbUrl := "https://micro.blog/micropub"
+	if media {
+		mbUrl += "/media"
+	}
+	mbUrl += "?mp-destination=" + url.QueryEscape(mpDestination)
+	request, err := http.NewRequest(http.MethodPost, mbUrl, body)
+	if err != nil {
+		return &http.Request{}, err
+	}
+	request.Header.Add("Authorization", "Bearer "+config.MicroBlog.Token)
+
+	return request, nil
+}
+
 // uploadFile takes the name of the file to upload, the destination blog, and
 // the Micro.blog API token, and uploads the file
-func uploadFile(filename string, mpDestination string, mbToken string) (string, error) {
-	mbUrl := "https://micro.blog/micropub/media?mp-destination=" + url.QueryEscape(mpDestination)
-
+func uploadFile(filename string, mpDestination string) (string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return "", err
@@ -33,11 +56,10 @@ func uploadFile(filename string, mpDestination string, mbToken string) (string, 
 	}
 	_ = writer.Close()
 
-	request, err := http.NewRequest(http.MethodPost, mbUrl, bytes.NewReader(body.Bytes()))
+	request, err := newMbRequest(mpDestination, true, bytes.NewReader(body.Bytes()))
 	if err != nil {
 		return "", err
 	}
-	request.Header.Add("Authorization", "Bearer "+mbToken)
 	request.Header.Add("Content-Type", writer.FormDataContentType())
 
 	client := &http.Client{}
@@ -50,27 +72,27 @@ func uploadFile(filename string, mpDestination string, mbToken string) (string, 
 	return resp.Header.Get("Location"), nil
 }
 
-func postMessage(message *Message, mpDestination string, mbToken string) (string, error) {
-	mbUrl := "https://micro.blog/micropub/media?mp-destination=" + url.QueryEscape(mpDestination)
-
+func postMessage(message *Message, mpDestination string) (string, error) {
 	data := url.Values{}
 	data.Set("h", "entry")
-	data.Set("content", url.QueryEscape(message.Text))
+	data.Set("content", message.Text)
 	data.Set("category", "txt")
-	for i := 0; i <= message.NumImages; i++ {
+	for i := 0; i < message.NumImages; i++ {
 		data.Add("photo[]", message.MBImageURLs[i])
 	}
 
-	request, err := http.NewRequest(http.MethodPost, mbUrl, strings.NewReader(data.Encode()))
+	request, err := newMbRequest(mpDestination, false, strings.NewReader(data.Encode()))
 	if err != nil {
 		return "", err
 	}
-	request.Header.Add("Authorization", "Bearer "+mbToken)
 
 	client := &http.Client{}
 	resp, err := client.Do(request)
 	if err != nil {
 		return "", err
+	}
+	if resp.StatusCode > 202 {
+		return "", errors.New(fmt.Sprintf("got a %d posting the message to Micro.blog", resp.StatusCode))
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
@@ -94,27 +116,25 @@ func postMessage(message *Message, mpDestination string, mbToken string) (string
 // UploadImagesToMicroBlog uploads all the images in the given Message to Micro.blog,
 // updating the Message with MBImageURLs.
 func UploadImagesToMicroBlog(message *Message) error {
-	config := LoadConfig()
-
-	destinationBlog := config.MicroBlog.Destination
-	if strings.HasPrefix(message.Text, "TEST: ") {
-		destinationBlog = config.MicroBlog.TestDestination
-	}
-
 	// this will skip if no images
 	for i := 0; i < message.NumImages; i++ {
-		mbUrl, err := uploadFile(message.ImageFilenames[i], destinationBlog, config.MicroBlog.Token)
+		mbUrl, err := uploadFile(message.ImageFilenames[i], destinationBlog(message))
 		if err != nil {
-			log.Printf("Error uploading file %q to blog %q", message.ImageFilenames[i], destinationBlog)
+			log.Printf("Error uploading file %q to blog %q", message.ImageFilenames[i], destinationBlog(message))
 			return err
 		}
 		message.MBImageURLs = append(message.MBImageURLs, mbUrl)
 	}
+	return nil
+}
 
+// UploadMessageToMicroBlog sends the text, including any image URLs in the given
+// Message to Micro.Blog, updating the MBPostURL with the resultant post.
+func UploadMessageToMicroBlog(message *Message) error {
 	var err error
-	message.MBPostURL, err = postMessage(message, destinationBlog, config.MicroBlog.Token)
+	message.MBPostURL, err = postMessage(message, destinationBlog(message))
 	if err != nil {
-		log.Printf("Error posting message to blog %q", destinationBlog)
+		log.Printf("Error posting message %q to blog %q: %s", message.Text, destinationBlog(message), err)
 		return err
 	}
 	return nil
